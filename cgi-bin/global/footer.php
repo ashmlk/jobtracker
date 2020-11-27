fa55cf2d801848cacf8a8be6550eab40e7477fd0
saif-alvi <56989215+saif-alvi@users.noreply.github.com>
2020-11-27 09:14:30 -0800

    Update footer.php

---
+++ cgi-bin/global/footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<title>Footer | JTracker</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<style>
+
+	</style>
+
+</head>
+
+<body>
+
+	<div class="container-fluid bg-dark pt-4 text-white">
+		
+		<div style="padding-bottom:35px;padding-top:10px" class="row">
+			
+			<div class="col-sm-3 text-center">
+				<h1 style="padding-top:25px" class="display-1">JTracker</h1>
+			</div>
+			
+			
+			<div class="col-sm-3 bordered border-left text-secondary">
+				<p style="padding-left:15px;padding-top:40px"> 
+				A job tracking web application where users can record and update details of jobs they have applied for. Users can keep track of company names, application dates, interviews, rejections and offers.</p>
+			</div>
+			
+			
+			<div class="col-sm-3 bordered text-center border-left">
+				<p>GO TO</p>
+				<p> <a href="" class="text-secondary">Home</a></p>
+				<p> <a href="about.html" class="text-secondary">About</a></p>
+				<p> <a href="contact.html" class="text-secondary">Contact</a></p>
+			</div>
+			
+			<div class="col-sm-3 bordered text-center border-left">
+				<p>CREATORS</p>
+				<div class = "pb-0">
+				<p> <a href="https://github.com/ashmlk" class="text-secondary">Arshia Malek</a></p>
+				<p> <a href="https://github.com/wtianchi" class="text-secondary">Tianchi Wu</a></p>
+				<p> <a href="https://github.com/ssaseun" class="text-secondary">Alex</a></p>
+				<p> <a href="https://github.com/saif-alvi" class="text-secondary">Saif Alvi</a></p>
+				</div>
+			</div>
+		
+		
+		</div>
+	
+	
+	
+	</div>
+	
+</body>
+
+</html>
