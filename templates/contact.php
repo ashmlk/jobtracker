f3e1d806a8846bd3142926d2e92f9891d01b5f7a
wtianchi <wtianchi@gmail.com>
2020-11-28 14:58:58 -0500

    Create contact.php

---
+++ templates/contact.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once "config.php";
+$query = "INSERT INTO contact (email, message) VALUES ('" . $_POST["email"] . "','" . $_POST["message"] . "')";
+$results = mysqli_query($link, $query);
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contact Us</title>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="bg-dark">
+    <div class="container" >
+        <div class="vg-white border-transparent card p-3 align-items-center">
+            <h3>Contact Us</h3>
+            <h2 class="mt-5">Your Message Has Been Send</h2>
+        </div>
+
+    </div>
+</body>
