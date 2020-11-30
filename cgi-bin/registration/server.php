57660714f5b1e36a0cd01fa5b798bcf04492ede9
Arshia Malek <arshiamalek@Arshias-MacBook-Pro.local>
2020-11-30 15:29:22 -0500

    "login with email and username added to backend"

--- cgi-bin/registration/server.php
+++ cgi-bin/registration/server.php
@@ -61,7 +61,7 @@ if (isset($_POST['login_user'])) {
   
     if (count($errors) == 0) {
         $password = md5($password);
-        $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+        $query = "SELECT * FROM user WHERE (username='$username' OR email='$username') AND password='$password'";
         $results = mysqli_query($link, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
